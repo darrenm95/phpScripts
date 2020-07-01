0c807fc9655082694ac70f65eceb4eaeae94b406
Darren Moore <darren.m95@icloud.com>
2020-07-01 11:08:09 +0100

    1D array example

---
+++ 1D_Array.php
@@ -0,0 +1,5 @@
+<?php
+//Note dollar sign at the start of variable name, '$' used to denote variables in PHP
+$animalArray =  array("Cow", "Sheep", "Dog", "Giraffe");
+echo "$animalArray[1]"; //PHP zero-indexed, index 1 refers to second element in array
+?>
