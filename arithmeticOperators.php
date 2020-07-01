50c42785f84ef21633de3a818e45b9746f76a93b
Darren Moore <darren.m95@icloud.com>
2020-07-01 12:03:30 +0100

    arithmetic operators

---
+++ arithmeticOperators.php
@@ -0,0 +1,20 @@
+<?php
+
+$addition = 5 + 4;
+$subtraction = 5 - 4;
+$multiplication = 5 * 4;
+$division = 5 / 4;
+$modulus = 5 % 4;
+
+$four = 4;
+
+$increment = ++$four;
+$decrement = --$four;
+
+echo "5 + 4 = " . $addition . "\n";
+echo "5 - 4 = " . $subtraction . "\n";
+echo "5 * 4 = " . $multiplication . "\n";
+echo "5 * 4 = " . $division . "\n";
+echo "5 % 4 = " . $modulus . "\n";
+echo '++$four = ' . $increment . "\n"; // single quotes do not expand variables
+echo '--$four = ' . $decrement . "\n"; // special characters are ignored, only characters that can be escaped by backslash is '\' and '\''
