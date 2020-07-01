2633969e2238cd9ea3627359e0adaa80917ebb63
Darren Moore <darren.m95@icloud.com>
2020-07-01 12:06:32 +0100

    arithmetic operators

--- arithmeticOperators.php
+++ arithmeticOperators.php
@@ -15,6 +15,6 @@ echo "5 + 4 = " . $addition . "\n";
 echo "5 - 4 = " . $subtraction . "\n";
 echo "5 * 4 = " . $multiplication . "\n";
 echo "5 * 4 = " . $division . "\n";
-echo "5 % 4 = " . $modulus . "\n";
+echo "5 % 4 = " . $modulus . "\n"; // gives remainder after number a has been split into groups of size, number b
 echo '++$four = ' . $increment . "\n"; // single quotes do not expand variables
 echo '--$four = ' . $decrement . "\n"; // special characters are ignored, only characters that can be escaped by backslash is '\' and '\''
