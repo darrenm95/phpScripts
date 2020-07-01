33e4d0e1ae37f6ae00ebdb73f80cebbdff4fcfc9
Darren Moore <darren.m95@icloud.com>
2020-07-01 14:48:32 +0100

    concatenate strings

---
+++ concatStrings.php
@@ -0,0 +1,10 @@
+<?php
+
+$string_1 = "Ghillies";
+$string_2 = " in the mist";
+
+$string_3 = $string_1 . $string_2 . "\n";
+$string_4 = "$string_1$string_2"; // Variables are substituted between double quotes
+
+echo $string_3;
+echo $string_4;
